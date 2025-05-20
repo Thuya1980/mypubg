58bd2e93a04d802a246a87995bac06d1d143ad8a
Thuya1980 <67178590+Thuya1980@users.noreply.github.com>
2025-05-20 23:42:58 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Install Chrome
 apt-get update
 apt-get install -y wget unzip curl
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Install Chromedriver
-CHROME_VERSION=$(google-chrome --version | grep -oP "\d+\.\d+\.\d+" | head -1)
-wget -N https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
+# Get Chrome major version
+CHROME_VERSION=$(google-chrome --version | grep -oP "\d+" | head -1)
+
+# Get matching chromedriver version for that major version
+CHROMEDRIVER_VERSION=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
+
+# Download chromedriver
+wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv chromedriver /usr/bin/chromedriver
 chmod +x /usr/bin/chromedriver
 
-# Run the app
 gunicorn app:app --bind 0.0.0.0:10000
